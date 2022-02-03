0261b17f353bf9c6d5fbe4b62a6dea247ebf4984
Arsenii <arsenii.mingazov@belstu.by>
2022-02-03 21:55:27 +0300

    Implemented the logic for the complete registration form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
 import Header from './components/nav/Header';
+import RegisterComplete from './pages/auth/RegisterComplete';
 
 const App = () => {
   return (
       <>
           <Header />
+          <ToastContainer />
           <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/register/complete" component={RegisterComplete} />
               <Route exact path="/login" component={Login} />
           </Switch>
       </>
