30f5e84266c6e165a7047d2aa1454fb932ec9da3
Arsenii <arsenii.mingazov@belstu.by>
2022-03-01 23:17:09 +0300

    Created history route

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import Home from './pages/Home';
 import Header from './components/nav/Header';
 import RegisterComplete from './pages/auth/RegisterComplete';
 import ForgotPassword from './pages/auth/ForgotPassword';
+import History from './pages/user/History';
+import UserRoute from './components/routes/UserRoute';
 
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
-import {createOrUpdateUser, currentUser} from './functions/auth';
+import { currentUser } from './functions/auth';
 
 const App = () => {
     const dispatch = useDispatch();
@@ -51,6 +53,7 @@ const App = () => {
                 <Route exact path="/register/complete" component={RegisterComplete} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/forgot/password" component={ForgotPassword} />
+                <UserRoute exact path="/user/history" component={History} />
             </Switch>
         </>
     );
