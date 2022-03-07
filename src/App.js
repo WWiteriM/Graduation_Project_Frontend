8b5d61cdc42bd2babc3a5796e683e07358f689d0
Arsenii <arsenii.mingazov@belstu.by>
2022-03-07 16:43:38 +0300

    Implemented create category page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
--- src/functions/category.js
+++ src/functions/category.js
@@ -8,7 +8,7 @@ export const getCategory = async (slug) => {
     return await axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
 };
 
-export const removeCategory = async (slug, category, authToken) => {
+export const removeCategory = async (slug, authToken) => {
     return await axios.delete(`${process.env.REACT_APP_API}/category/${slug}`, {
         headers: {
             authToken
